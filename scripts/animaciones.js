0c5b4fee48a86164043b1d44f4056ae7f0631718
Dilian Us <dilian0217@gmail.com>
2022-06-26 20:21:10 -0500

    trabajoDomingo

---
+++ scripts/animaciones.js
@@ -0,0 +1,7 @@
+const navToggle = document.getElementById("nav-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+
+navToggle.onclick = function (){
+    document.navMenu.style.left = "0";
+}
