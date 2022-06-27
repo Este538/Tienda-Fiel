bfc90b480b43b3dc70e0040276173ec5a01bdfb5
Dilian Us <dilian0217@gmail.com>
2022-06-26 21:49:42 -0500

    sliderv1

--- scripts/animaciones.js
+++
@@ -1,7 +0,0 @@
-const navToggle = document.getElementById("nav-toggle");
-const navMenu = document.getElementById("nav-menu");
-
-
-navToggle.onclick = function (){
-    document.navMenu.style.left = "0";
-}
