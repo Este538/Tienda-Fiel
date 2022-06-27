5ae7099343e9e4ede1792b7f3b9dc66ce46b9e52
Dilian Us <dilian0217@gmail.com>
2022-06-27 02:52:50 -0500

    v8

---
+++ scripts/animaciones.js
@@ -0,0 +1,24 @@
+const imgArray = [
+    "/images/Catálogo.png",
+    "/images/Desktop - 1.png"
+    
+];
+
+const img = document.getElementById("slide");
+
+let i = 0;
+
+const slideShow = () =>{
+    img.src = imgArray[i];
+    if(i<imgArray.length - 1){
+        i=i+1;
+    }else{
+        i = 0;
+    }
+}
+
+window.onload = function (){
+    setInterval(slideShow, 5000)
+};
+
+    
