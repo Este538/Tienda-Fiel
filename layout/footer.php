e18644f19a6742de7eb7286568ab50400a3cc7e1
Dilian Us <dilian0217@gmail.com>
2022-07-03 11:43:03 -0500

    cambio a php

---
+++ layout/footer.php
@@ -0,0 +1,10 @@
+     <footer>
+        <div class="footer-container">
+            <p>&copy;2022 CitrusConect</p>
+        </div>
+    </footer>
+    <!--Scripts-->
+    <script src="/scripts/animaciones.js"></script>
+    
+</body>
+</html>
---
+++ layout/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="../style/style.css">
+    <script src="https://kit.fontawesome.com/5edc3a915f.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1.7.7/glider.min.css">
+    <link rel="stylesheet" href="../style/style.css"> 
+    <!--Ultimo commit unificado--> 
+</head>
+<body>
+    <header >
+        <div class="container-nav">
+            
+            <div class="logo">
+                <a href="../public/index.html" class="logo">logo</a>
+            </div>
+            <nav>
+                <ul class="nav-menu" id="nav-menu">
+
+                    <li class="nav-menu-item"><a href="catalogo.html" class="nav-menu-link"><i class="fa-solid fa-lemon"></i>Catálogo</a></li>
+                    <li class="nav-menu-item"><a href="contacto.html" class="nav-menu-link"><i class="fa-solid fa-lemon"></i>Contacto</a></li>
+                    <li class="nav-menu-item"><a href="" class="nav-menu-link"><i class="fa-solid fa-lemon"></i>Blog</a></li>
+                    
+                </ul>
+            </nav>
+            <div class="carrito">
+                <button id="search" class="search">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    
+                </button>
+                <button id="registro" class="registro">
+                    <a href="login.html"> <h3>Registrate</h3></a>
+                   
+                </button>
+                <button id="carritoIcon" class="carritoIcon">
+                    <a href="carrito.html"><i class="fa-solid fa-cart-shopping"></i></a>
+                    
+                </button>
+            </div>
+        </div>
+    </header>
